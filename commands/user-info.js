f5105cfec6ab894aa1e5622d610b1e5ed0de4940
piyushchauhan <pi.codemonk@gmail.com>
2020-07-20 11:49:30 +0530

    Minor changes

--- commands/user-info.js
+++ commands/user-info.js
@@ -1,9 +1,12 @@
 module.exports = {
 	name: 'user-info',
 	description: 'Display info about yourself.',
-	execute(message) {
-		message.channel.send(`Your username: ${message.author.username}:${message.author.tag}\n
-		Your ID: ${message.author.id}`);
+	execute(message, args) {
+		console.log(`${message}\n${args.toString()}`);
+		let userName = message.author.tag;
+		message.channel.send(`Your username: ${userName}\n`);
 		// TODO fetch the roll number from the username and tag
+		// var role = message.guild.roles.find(role => role.name === "role-b");
+		// message.member.addRole(role);
 	},
 };
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix, token } = require('./config.json');
+const { prefix, token } = require('./configs/config.json');
+
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -13,6 +14,16 @@ for (const file of commandFiles) {
 client.once('ready', () => {
     console.log('Ready!');
 });
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Welcome to the server, ${member}`);
+  });
 
 client.on('message', message => {
     console.log(`Message string:${message.toString()}\nSent by:${message.author}`);
