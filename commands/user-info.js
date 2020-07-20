831ab6862a135d04c60235522c789557962985f7
piyushchauhan <pi.codemonk@gmail.com>
2020-07-20 12:32:20 +0530

    Google spreadsheets working

--- commands/user-info.js
+++ commands/user-info.js
@@ -3,8 +3,8 @@ const {
     getSpreadSheetValues
   } = require('./googleSheetsService.js');
 
-const spreadsheetId = "<put spreadsheet id here>";
-const sheetName = "<put sheet name here>";
+const spreadsheetId = "1tLG5wq2MRHDmBVe1FJyTTVO8ABUWy67emr-45--dzbk";
+const sheetName = "allStudents";
 
 async function getDetails(message){
 	var author_detail = null;
@@ -20,6 +20,7 @@ async function getDetails(message){
 	catch (error) {
 		console.log(error);
 	};
+	console.log(`Finding match for ${message.author.username}`);
 	for(var i in author_detail){
 		if (author_detail[i][0] == message.author.username){
 			message.channel.send(`Your Name is ${author_detail[i][1]}`);
@@ -34,19 +35,13 @@ async function getDetails(message){
 module.exports = {
 	name: 'user-info',
 	description: 'Display info about yourself.',
-<<<<<<< HEAD
 	execute(message, args) {
 		console.log(`${message}\n${args.toString()}`);
 		let userName = message.author.tag;
 		message.channel.send(`Your username: ${userName}\n`);
 		// TODO fetch the roll number from the username and tag
+		getDetails(message);
 		// var role = message.guild.roles.find(role => role.name === "role-b");
 		// message.member.addRole(role);
-=======
-	execute(message) {
-		message.channel.send(`Your username: ${message.author.username}:${message.author.tag}\n
-		Your ID: ${message.author.id}`);
-		getDetails(message);
->>>>>>> 4282f8f2fa613226e99c179f32d7b381b500cf12
 	},
 };
