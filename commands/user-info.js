78133e343e0c90c548ad1e21dae28f90b239dd1f
piyushchauhan <pi.codemonk@gmail.com>
2020-07-21 16:09:26 +0530

    Added showing of house

--- commands/user-info.js
+++ commands/user-info.js
@@ -1,14 +1,14 @@
 const {
-    getAuthToken,
-    getSpreadSheetValues
-  } = require('../services/googleSheetsService.js');
+	getAuthToken,
+	getSpreadSheetValues
+} = require('../services/googleSheetsService.js');
 
 const spreadsheetId = "1tLG5wq2MRHDmBVe1FJyTTVO8ABUWy67emr-45--dzbk";
 const sheetName = "allStudents";
 
-async function getDetails(message){
+async function getDetails(message) {
 	var author_detail = null;
-	try{
+	try {
 		const auth = await getAuthToken();
 		const details = await getSpreadSheetValues({
 			spreadsheetId,
@@ -21,14 +21,32 @@ async function getDetails(message){
 		console.log(error);
 	};
 	console.log(`Finding match for ${message.author.tag}`);
-	for(var i in author_detail){
-		if (author_detail[i][0] == message.author.tag){
-			message.channel.send(`Your Name is ${author_detail[i][1]}`);
-			message.channel.send(`Your Roll No is ${author_detail[i][2]}`);
+	for (var i in author_detail) {
+		
+		let discordUserNameTag = author_detail[i][0];
+		if (discordUserNameTag == message.author.tag) {
+			let realName = author_detail[i][1];
+			let rollNumber = author_detail[i][2];
+			var house = author_detail[i][4];
+
+			if (house === 'A') {
+				house = 'Auriga';
+			} else if (house === 'C') {
+				house = 'Cassiopeia';
+			} else if (house === 'P') {
+				house = 'Pegasus';
+			} else if (house === 'D') {
+				house = 'Darco';
+			}
+
+			message.channel.send(`Your Name is ${realName}\nYour Roll No is ${rollNumber}\nYour house is ${house}`);
+			// TODO Do not print the roll number
+			// TODO Assign a role based on the roll number like give `role-a` if rollNumber starts with 1901
 			return;
 		}
 	};
-	message.channel.send("no rollno found in database");
+	message.reply("Sorry we couldn't find you in our database. \
+	Please ping @moderator to identify you.");
 	return;
 }
 
@@ -38,8 +56,11 @@ module.exports = {
 	execute(message, args) {
 		console.log(`${message}\n${args.toString()}`);
 		let userName = message.author.tag;
+		// TODO Check for mentions in this command
+		// If someone runs !user-info @dhushyanth in the message 
+		// then identify the mentioned user and fetch the 
+		// roll number of that user
 		message.channel.send(`Your username: ${userName}\n`);
-		// TODO fetch the roll number from the username and tag
 		getDetails(message);
 		// var role = message.guild.roles.find(role => role.name === "role-b");
 		// message.member.addRole(role);
